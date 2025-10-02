24f6fb13ecb1a7bc162af999bf1d1eb2a8f31466
typeofadam <typeof.adam@gmail.com>
2025-10-02 15:42:46 +0100

    created stats file for stats funcs

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import get_word_count
+
 def get_book_text(file_path):
     with open(file_path) as book:
         return book.read()
-
-def get_word_count(string_to_count):
-    word_list = string_to_count.split()
-    return len(word_list)
 
 def main():
     frankenstein_path = "./books/frankenstein.txt"
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(string_to_count):
+    word_list = string_to_count.split()
+    return len(word_list)
