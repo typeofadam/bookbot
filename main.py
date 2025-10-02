c5eb6f4f3d8410f44c50ed85329f6608e5efcd7b
typeofadam <typeof.adam@gmail.com>
2025-10-02 16:33:21 +0100

    changed to use sys and to remove debug print

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from stats import get_word_count, get_char_counts, get_char_sorted_dict_list
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as book:
         return book.read()
 
 def main():
-    frankenstein_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    frankenstein_path = sys.argv[1]
     frankenstein_text = get_book_text(frankenstein_path)
     num_words = get_word_count(frankenstein_text)
     char_counts = get_char_counts(frankenstein_text)
     char_dict_list = get_char_sorted_dict_list(char_counts)
     
-    print(char_counts, char_dict_list)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {frankenstein_path}...")
     print("----------- Word Count ----------")
