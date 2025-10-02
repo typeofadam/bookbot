d637cc272a9024b1ac1ad1b05e20005571969033
typeofadam <typeof.adam@gmail.com>
2025-10-02 15:33:11 +0100

    Created function that opens and reads a file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as book:
+        return book.read()
+
+def main():
+    frankenstein_path = "./books/frankenstein.txt"
+    print(get_book_text(frankenstein_path))
+
+main()
