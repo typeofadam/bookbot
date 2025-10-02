283b641152ea523b1bc0318c7e8034b5d9631222
typeofadam <typeof.adam@gmail.com>
2025-10-02 15:56:42 +0100

    created char counter, but used built in count()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, get_char_counts
 
 def get_book_text(file_path):
     with open(file_path) as book:
@@ -6,7 +6,9 @@ def get_book_text(file_path):
 
 def main():
     frankenstein_path = "./books/frankenstein.txt"
-    num_words = get_word_count(get_book_text(frankenstein_path))
+    frankenstein_text = get_book_text(frankenstein_path)
+    num_words = get_word_count(frankenstein_text)
     print(f"Found {num_words} total words")
+    print(get_char_counts(frankenstein_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
 def get_word_count(string_to_count):
     word_list = string_to_count.split()
     return len(word_list)
+
+def get_char_counts(string_to_count):
+    string_to_count = string_to_count.lower()
+    char_counts = {}
+    char_set = set(string_to_count)
+    for char in char_set:
+        char_counts[char] = string_to_count.count(char)
+    return char_counts
