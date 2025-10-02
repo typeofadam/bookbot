bde1fdb766dc787726dc21305464653b2df01c88
typeofadam <typeof.adam@gmail.com>
2025-10-02 16:27:37 +0100

    addeded fancy printing and dict to list func

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from stats import get_word_count, get_char_counts
+from stats import get_word_count, get_char_counts, get_char_sorted_dict_list
 
 def get_book_text(file_path):
     with open(file_path) as book:
         return book.read()
 
 def main():
-    frankenstein_path = "./books/frankenstein.txt"
+    frankenstein_path = "books/frankenstein.txt"
     frankenstein_text = get_book_text(frankenstein_path)
     num_words = get_word_count(frankenstein_text)
+    char_counts = get_char_counts(frankenstein_text)
+    char_dict_list = get_char_sorted_dict_list(char_counts)
+    
+    print(char_counts, char_dict_list)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {frankenstein_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(get_char_counts(frankenstein_text))
+    print("--------- Character Count -------")
+
+    for char in char_dict_list:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,13 @@ def get_char_counts(string_to_count):
     for char in char_set:
         char_counts[char] = string_to_count.count(char)
     return char_counts
+
+def sort_on(items):
+    return items["num"]
+
+def get_char_sorted_dict_list(char_dict):
+    char_dict_list = []
+    for key, value in char_dict.items():
+        char_dict_list.append({"char": key, "num": value})
+    char_dict_list.sort(reverse=True, key=sort_on)
+    return char_dict_list
