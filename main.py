9c39a8803cb2939b1aa4aa7acea7988a5b090a3b
typeofadam <typeof.adam@gmail.com>
2025-10-02 15:38:11 +0100

    added func that counts words in string

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def get_book_text(file_path):
     with open(file_path) as book:
         return book.read()
 
+def get_word_count(string_to_count):
+    word_list = string_to_count.split()
+    return len(word_list)
+
 def main():
     frankenstein_path = "./books/frankenstein.txt"
-    print(get_book_text(frankenstein_path))
+    print(get_word_count(get_book_text(frankenstein_path)))
 
 main()
